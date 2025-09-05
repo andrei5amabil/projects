31876461edc91754dfdcf78b06fde177dad26379
Andrei Popescu <andrei69420@yahoo.com>
2025-09-05 13:53:05 +0300

    delete from html render when delete

--- amzncopy/javascript-amazon-project/scripts/checkout.js
+++ amzncopy/javascript-amazon-project/scripts/checkout.js
@@ -19,7 +19,7 @@ cart.forEach( (cartItem) => {
     console.log(matchingProduct);
 
     cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -106,7 +106,9 @@ document.querySelectorAll('.js-delete-link').forEach(
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
             removeFromCart(productId);
-            
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            console.log(container);
+            container.remove();
         });
     }
 );
