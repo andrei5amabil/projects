211c4776e3d8b1fe3c70c5c806a6979e23988081
Andrei Popescu <andrei69420@yahoo.com>
2025-09-05 17:26:07 +0300

    update button stuff

--- amzncopy/javascript-amazon-project/scripts/checkout.js
+++ amzncopy/javascript-amazon-project/scripts/checkout.js
@@ -54,9 +54,11 @@ cart.forEach( (cartItem) => {
                 <span>
                 Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                 </span>
-                <span class="update-quantity-link link-primary">
+                <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
                 Update
                 </span>
+                <input class="quantity-input invisible">
+                <span class="save-quantity-link link-primary">Save</span>
                 <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                 Delete
                 </span>
@@ -129,3 +131,14 @@ document.querySelectorAll('.js-delete-link').forEach(
     }
 );
 
+document.querySelectorAll('.js-update-link').forEach(
+    (link) => {
+        link.addEventListener('click', () => {
+            const productId = link.dataset.productId;
+            console.log(productId);
+
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            container.classList.add('is-editing-quantity');
+        })
+    }
+)
