f16811f80c0faba63d83bf1429a9cbe5bc57584f
Andrei Popescu <andrei69420@yahoo.com>
2025-09-24 18:44:40 +0300

    added routes

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
 import { HomePage } from './pages/HomePage'
+import { Routes, Route } from 'react-router'
 
 function App() {
 
   return (
-    <HomePage />
+    <Routes>
+      <Route 
+        path="/"
+        element={<HomePage />}
+      />
+      <Route 
+        path="/checkout"
+        element={<h1>Checkout Page</h1>}
+      ></Route>
+    </Routes>
   )
 }
 
