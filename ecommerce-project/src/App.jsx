3b359a2b2be2032ae105077e82ad8e6038c62e90
Andrei Popescu <andrei69420@yahoo.com>
2025-09-30 10:49:59 +0300

    minor changes

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import { HomePage } from './pages/HomePage'
 import { Routes, Route } from 'react-router'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 
 function App() {
 
@@ -14,6 +16,14 @@ function App() {
       <Route 
         path="/checkout"
         element={<CheckoutPage />}
+      ></Route>
+      <Route 
+        path="/orders"
+        element={<OrdersPage />}
+      ></Route>
+      <Route 
+        path="/tracking"
+        element={<TrackingPage />}
       ></Route>
     </Routes>
   )
